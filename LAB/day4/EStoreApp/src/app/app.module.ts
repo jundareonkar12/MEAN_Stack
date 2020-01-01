a2ba22e3b8781a41d7e25210b4598f76236480c7
jundareonkar12 <jundare.onkar12@gmail.com>
2020-01-01 14:15:51 +0530

    day-4 assign completed

--- LAB/day4/EStoreApp/src/app/app.module.ts
+++ LAB/day4/EStoreApp/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';//add this for using ng-model
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent
+    AboutComponent,
+    HomeComponent,
+    ListComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule//add forms module here so that u can use ng model in "app.component.html"
   ],
   providers: [],
   bootstrap: [AppComponent]
